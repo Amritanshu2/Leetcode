e103ff23bad3293c8c876089d5bc3797e89a2d85
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2023-03-25 18:33:29 +0530

    Time: 45 ms (75.21%), Space: 55.3 MB (72.13%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -10,13 +10,7 @@ class Solution {
                 diff = nums[i+1]-nums[i];
             }
         }
-        if(diff == -1){
-            for(int i = 0;i<nums.length-1;i++){
-            if(Math.abs(nums[i+1]-nums[i])>diff){
-                diff = Math.abs(nums[i+1]-nums[i]);
-            }
-        }
-        }
+        
         return diff;
     }
 }
