acfa578549e6370f2dc8d2d0f69cc2474b0c64a9
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2023-07-26 13:19:06 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (41.10%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,13 +1,5 @@
 class Solution {
     public String convertToBase7(int num) {
-        if(num == 0){return "0";}
-        String f ="";
-        String s = "";if(num<0){f = "-";}
-        num = Math.abs(num);
-        while(num>0){
-            s =  num%7 + s;
-            num /= 7;
-        }
-        return f+s;
+        return Integer.toString(num, 7);
     }
 }
