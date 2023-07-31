b0c39f988c38afca592c87ede38b7a199eb19fb6
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2023-07-31 08:58:44 +0530

    Added solution - LeetHub

--- Overlapping Intervals - GFG/overlapping-intervals.java
+++ Overlapping Intervals - GFG/overlapping-intervals.java
@@ -40,37 +40,35 @@ class Solution
     public int[][] overlappedInterval(int[][] in)
     {
         // Code here // Code here
-        Stack<int[]> st = new Stack<>();
+        Stack<int[]>st = new Stack<>();
         Arrays.sort(in,(a,b)->{
             return a[0]-b[0];
         });
         st.push(in[0]);
         for(int i = 1;i<in.length;i++){
-            int[]t = st.pop();
-            int s1 = t[0];
-            int e1 = t[1];
-            
-            int s2 = in[i][0];
-            int e2 = in[i][1];
-            int em = Math.max(e1,e2);
-            
-            if(s2<=e1){
-                int[] merge ={s1,em};
-                st.push(merge);
-            }
-            
-            else{
-                st.push(t);
-                st.push(in[i]);
-            }
-            
+               int[]arr = st.pop();
+               int s1 = arr[0];
+               int e1 = arr[1];
+               
+               int s2 = in[i][0];
+               int e2 = in[i][1];
+               
+               if(s2<=e1){
+                   int em = Math.max(e1,e2);
+                   int[]ans = {s1,em};
+                   st.push(ans);
+               }
+               else{
+                   st.push(arr);
+                   st.push(in[i]);
+               }
+               
         }
-        int[][]ans = new int[st.size()][2];
-        for(int i = ans.length-1;i>=0;i--){
-            int[]tm = st.pop();
-            ans[i][0] = tm[0];
-            ans[i][1] = tm[1];
+        
+        int [][] arr = new int[st.size()][2];
+        for(int i = st.size()-1;i>=0;i--){
+            arr[i] = st.pop();
         }
-        return ans;
+        return arr;
     }
 }
