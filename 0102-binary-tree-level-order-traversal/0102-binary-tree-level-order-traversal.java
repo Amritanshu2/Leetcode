83a85d3a6565fe2511e5544d55f8ea89b3e4276f
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2023-08-30 16:29:24 +0530

    Time: 1 ms (94.22%), Space: 44.2 MB (43.03%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public HashMap<Integer,ArrayList<Integer>> ans(TreeNode root , HashMap<Integer, ArrayList<Integer>> hm , int n ){
+        if(root == null){
+            return hm;
+        }
+        if(hm.containsKey(n)){
+            ArrayList al = hm.get(n);
+            al.add(root.val);
+            hm.put(n,al);
+        }
+        else{
+            ArrayList<Integer> al =  new ArrayList<>();
+            al.add(root.val);
+            hm.put(n,al);
+            
+        }
+        ans(root.left,hm,n+1);
+        ans(root.right,hm,n+1);
+        return hm;
+        
+        
+    }
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        HashMap<Integer,ArrayList<Integer>> hm = new HashMap<>();
+        hm = ans(root,hm,0);
+        List<List<Integer>> al = new ArrayList<>();
+        for(int i = 0;i<hm.size();i++){
+            al.add(hm.get(i));
+        }
+        return al;
+    }
+}
