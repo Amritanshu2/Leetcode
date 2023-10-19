d0a4e58b090a300c49ca40c7e355127333531911
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2023-10-19 23:57:12 +0530

    Time: 5 ms (9.90%), Space: 41 MB (11.39%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,26 +1,29 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        Stack<Character> st = new Stack<>();
-        Stack<Character> ts = new Stack<>();
+    public String rom(String s){
+        Stack<Character>st = new Stack<>();
         for(int i = 0;i<s.length();i++){
-            if(st.empty()!= true && s.charAt(i)=='#'){
+            if(s.charAt(i) == '#' && st.size()>0){
                 st.pop();
             }
-            else if(s.charAt(i)!='#'){st.push(s.charAt(i));}
-        }
-        for(int i = 0;i<t.length();i++){
-            if(ts.empty()!= true && t.charAt(i)=='#'){
-                ts.pop();
+            else if (s.charAt(i)!= '#'){
+                st.push(s.charAt(i));
             }
-            else if(t.charAt(i)!='#'){ts.push(t.charAt(i));}
         }
-        if(st.size()!=ts.size()){return false;}
-        while(st.empty()!= true && ts.empty()!= true){
-            if(st.pop()!=ts.pop()){return false;}
+        s = "";
+        while(st.size()>0){
+            s  = st.pop() +s;
+        }
+        System.out.println(s);
+        return s;
+    }
+    public boolean backspaceCompare(String s, String t) {
+        String a = rom(s);
+        String b = rom(t);
+        if(a.length()!= b.length()){return false;}
+        for(int i = 0;i<a.length();i++){
+            if(a.charAt(i)!=b.charAt(i)){return false;}
         }
         return true;
-        
-        
         
     }
 }
