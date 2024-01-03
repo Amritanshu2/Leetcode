9d269a2bba83e5bd40b0c99c83086551eea7e9df
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-01-03 11:31:30 +0530

    Time: 1 ms (78.99%), Space: 41.8 MB (26.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,28 @@
-class Solution {
-    
-    HashMap<Character,String> hm = new HashMap<>();
-    List<String>al = new ArrayList<>();
-    public void sh(){
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
+class Solution 
+{
+    String[] keypad={"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) 
+    {
+        List<String> combinations=new ArrayList<>();
+        if(digits.length()==0)
+            return combinations;
+        StringBuilder helper=new StringBuilder();
+        calculate(digits,0,helper,combinations);    
+        return combinations;   
     }
-    
-    public void num(String nums,String s, int idx){
-        if(s.length()==nums.length() && s.length()>0){al.add(s);}
-        if(idx >= nums.length()){return;}
-        
-        String ss = hm.get(nums.charAt(idx));
-        for(int i = 0;i<ss.length();i++){
-            num(nums,s+ss.charAt(i),idx+1);
+    public void calculate(String digits,int index,StringBuilder helper,List<String> combinations)
+    {
+        if(index==digits.length())
+        {
+            combinations.add(helper.toString());
+            return ;
         }
-    }
-    
-    public List<String> letterCombinations(String digits) {
-        sh();String a = "";
-        num(digits,a,0);
-        
-        return al;
+        int digit=digits.charAt(index)-'0';
+        for(int i=0;i<keypad[digit].length();i++)
+        {
+            helper.append(keypad[digit].charAt(i));
+            calculate(digits,index+1,helper,combinations);
+            helper.deleteCharAt(helper.length()-1);
+        }
     }
 }
