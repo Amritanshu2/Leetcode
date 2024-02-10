6aa2f89de1756878b41393183434882e061918d8
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-02-10 22:19:07 +0530

    Time: 35 ms (9.50%), Space: 51.1 MB (16.33%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,40 +1,29 @@
 class Solution {
+    int[][]arr;
     public int longestCommonSubsequence(String text1, String text2) {
-        int len1=text1.length();
-        int len2=text2.length();
-        char ch1[]=text1.toCharArray();
-        char ch2[]=text2.toCharArray();
-
-        int dp[][]=new int[len1][len2];
-        
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+        arr = new int[text1.length()+1][text2.length()+1];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = -1;
+            }
         }
-
-        return solve(ch1,ch2,dp,len1-1,len2-1);
+        return longest(text1, 0, text2, 0);
     }
-    public int solve(char ch1[],char ch2[],int dp[][],int index1,int index2){
-    //base case
-    if(index1<0 || index2<0)return 0;
     
-    //Dp
-    if(dp[index1][index2]!=-1)return dp[index1][index2];
-
-    //match 
-    if(ch1[index1]==ch2[index2]){
-        dp[index1][index2]=1+solve(ch1,ch2,dp,index1-1,index2-1);
-    }
-    //not match
-    else{
-        //not considering from ch1
-        int nt1=solve(ch1,ch2,dp,index1-1,index2);
-        //not considering from ch2
-        int nt2=solve(ch1,ch2,dp,index1,index2-1);
-
-        //find max in (nt1,nt2) then assign it to dp[index1][index2];
-        dp[index1][index2]=Math.max(nt1,nt2);
-    }
-
-    return dp[index1][index2];
+    public int longest(String s1, int i, String s2, int j){
+        if(i>=s1.length() || j>=s2.length()){return 0;}
+        
+        if(arr[i][j]!=-1){return arr[i][j];}
+        
+        if(s1.charAt(i) == s2.charAt(j)){
+            return arr[i][j] = longest(s1,i+1,s2,j+1)+1;
+        }
+        
+        else{
+            int a = longest(s1,i,s2,j+1);
+            int b = longest(s1,i+1,s2,j);
+            return arr[i][j] = Math.max(a,b);
+        }
+        
     }
 }
