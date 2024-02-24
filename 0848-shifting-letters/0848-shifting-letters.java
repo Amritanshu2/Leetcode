2ec58bd3ef2a272558ec4d45b92d81825d502fa4
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-02-24 09:35:05 +0530

    Time: 706 ms (18.11%), Space: 56.8 MB (66.32%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -8,11 +8,11 @@ class Solution {
         int totalShift = 0;
         StringBuilder result = new StringBuilder();
 
-        // Accumulate shifts in reverse order
+       
         for (int i = shifts.length - 1; i >= 0; i--) {
             totalShift = (totalShift + shifts[i]) % 26;
             int index = (s.charAt(i) - 'a' + totalShift) % 26;
-            result.insert(0, characters[index]); // Insert at the beginning to maintain order
+            result.insert(0, characters[index]); 
         }
 
         return result.toString();
