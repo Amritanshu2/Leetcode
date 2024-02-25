fa12d043a645502257121951fc4fa7f39640bbe5
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-02-25 21:00:25 +0530

    Time: 3 ms (8.97%), Space: 48.4 MB (8.36%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int hIndex = 0 ; 
+        for(int i = 0 ; i < citations.length ; i++){
+            int currentHindex = Math.min(citations[i] , citations.length - i );
+            hIndex = Math.max(hIndex , currentHindex);
+        }
+        return hIndex; 
+    }
+}
