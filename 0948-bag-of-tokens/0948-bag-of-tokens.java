2e2c30f2a04c22a7a43d79eec5155202c55528b9
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-03-05 00:28:41 +0530

    Time: 8 ms (5.73%), Space: 44.3 MB (5.41%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,26 +1,40 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        int n = tokens.length;
-        int ans = 0;
-        
-        if (n == 1) {
-            return power >= tokens[0] ? 1 : 0;
+        ArrayList<Integer> al = new ArrayList<>();
+
+        for (Integer element : tokens) {
+            al.add(element);
+        }
+        Collections.sort(al);
+        check(al,power,0);
+        return a;
+    }
+    int a  = 0;
+    public void check(ArrayList<Integer>al,int power, int score){
+        a = Math.max(a,score);
+        if(al.size() == 0){return;}
+        if(power < al.get(0) && score == 0){return;}
+       
+        if(power < al.get(0) && score>0){
+            
+            int s = al.get(al.size()-1);
+            al.remove(al.size()-1);
+            
+            
+            check(al,power+s,score-1);
+            al.add(s);
+            
         }
         
-        Arrays.sort(tokens);
-        int low = 0, high = n - 1;
-        
-        while (low <= high) {
-            if (power >= tokens[low]) {
-                power -= tokens[low++];
-                ans++;
-            } else if (ans > 0 && high > low) {
-                power += tokens[high--];
-                ans--;
-            } else {
-                break;
-            }
+        if(power >= al.get(0)){
+            
+            int s = al.get(0);
+            al.remove(0);
+            
+            
+            check(al,power-s,score+1);
+            al.add(0,s);
+            
         }
-        
-        return ans;
-    }}
+    }
+}
