d11c01adc1b2e8bacaabdbd83f5a6f3eb631b540
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-03-08 01:31:39 +0530

    Time: 4 ms (100.00%), Space: 81.4 MB (80.41%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,33 +10,24 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode temp = head;
-        while(temp.next!=null){
-            if(temp.val == 0){
-                ListNode remp = new ListNode(0);
-                temp.next = hlpr(temp.next,remp);
+        
+        ListNode temp = head.next;
+        ListNode prev = head;
+        int sum = 0;
+        while(temp!=null){
+            if(temp.val!=0){
+                sum = sum + temp.val;
+            }
+            else{
+                prev.next = temp;
+                temp.val = sum;
+                prev = temp;
+                sum = 0;
             }
             temp = temp.next;
         }
-        temp = head;ListNode remp = temp;
-        while(temp != null){
-            if(temp == head && temp.val == 0){head = head.next;temp = head;}
-            if(temp.next == null && temp.val == 0){remp.next = null;break;}
-            if(temp.val == 0){remp.next = temp.next;temp.next = null;temp = remp;}
-            remp = temp;
-            temp = temp.next;
-            
-        }
-       
-        return head;
+        return head.next;
     }
     
-    public ListNode hlpr(ListNode head,ListNode temp){
-        while(head.val != 0){
-            temp.val = temp.val+head.val;
-            head = head.next;
-        }
-        temp.next = head;
-        return temp;
-    }
+    
 }
