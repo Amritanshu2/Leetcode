9b3f4b49bc4a269f7e931400381ffde5efbd993e
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-05-14 02:18:53 +0530

    Time: 3 ms (96.60%), Space: 45.5 MB (19.73%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
@@ -1,57 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    HashMap<Integer,Integer>hm = new HashMap<>();
-    
-    public int trav(TreeNode root){
-        if(root == null){return 0;}
-        int a = trav(root.left);
-        int b = trav(root.right);
-        hm.put(a+b+root.val,hm.getOrDefault(a+b+root.val,0)+1);
-        return a+b+root.val;
-        
+    HashMap<Integer, Integer> hm = new HashMap<>();
+
+    public int trav(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftSum = trav(root.left);
+        int rightSum = trav(root.right);
+        int sum = leftSum + rightSum + root.val;
+
+        hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+
+        return sum;
     }
-    
+
     public int[] findFrequentTreeSum(TreeNode root) {
-        trav(root);int max = 0;int sum = 0;
+        trav(root);
+
+        List<Integer> result = new ArrayList<>();
+        int maxFrequency = 0;
+
         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            
-            // max = Math.max(max,entry.getValue());
-            if(max<entry.getValue()){sum = entry.getKey();max = entry.getValue();}
-            
-        }
-        int a = 0;
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if(max == entry.getValue()){a++;}
-        }
-        if(a>1){
-                List<Integer>al = new ArrayList<>();
-                for (Map.Entry<Integer, Integer> ent : hm.entrySet()) {
-                    if(ent.getValue() == max){al.add(ent.getKey());}
-                    
-                }
-            int[] array = new int[al.size()];
-            for(int i = 0;i<al.size();i++){
-                array[i] = al.get(i);
+            int frequency = entry.getValue();
+            if (frequency > maxFrequency) {
+                result.clear();
+                result.add(entry.getKey());
+                maxFrequency = frequency;
+            } else if (frequency == maxFrequency) {
+                result.add(entry.getKey());
             }
-                return array;
-                                       }
-        
-        int[]arr = {sum};
-        return arr ;
-        
+        }
+
+        int[] resArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resArray[i] = result.get(i);
+        }
+
+        return resArray;
     }
 }
