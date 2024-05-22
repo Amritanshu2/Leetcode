40f2c84020e201402a8710c47b15e22c38cf3901
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-05-22 16:08:36 +0530

    Time: 37 ms (5.06%), Space: 56.1 MB (84.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,32 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 class Solution {
     
     List<List<String>> ans = new ArrayList<>();
-    HashMap<String,Boolean>hm = new HashMap<>();
+    HashMap<String, Boolean> hm = new HashMap<>();
     
     public List<List<String>> partition(String s) {
-        part(s,new ArrayList<>(),0,"");
+        part(s, new ArrayList<>(), 0, "");
         return ans;
     }
     
-    public void part(String s, List<String>al , int i, String as){
-        if(i == s.length()){
-            int a = 0;
-            if(as.length()>0){al.add(as);a++;}
-            if(check(al)){ans.add(new ArrayList<>(al));}
-            if(a>0) al.remove(al.size()-1);
+    public void part(String s, List<String> al, int i, String as) {
+        if (i == s.length()) {
+            if (!as.isEmpty()) {
+                al.add(as);
+            }
+            if (check(al)) {
+                ans.add(new ArrayList<>(al));
+            }
+            if (!as.isEmpty()) {
+                al.remove(al.size() - 1);
+            }
             return;
         }
         
+        // Recursive call without adding a new segment
+        part(s, al, i + 1, as + s.charAt(i));
         
-        
-        if(as.length()!=0){
+        // Recursive call with adding a new segment if as is not empty
+        if (!as.isEmpty()) {
             al.add(as);
-            part(s,al,i+1,""+s.charAt(i));
-            al.remove(al.size()-1);
+            part(s, al, i + 1, "" + s.charAt(i));
+            al.remove(al.size() - 1);
         }
-        
-        part(s,al,i+1,as+s.charAt(i));
-        
     }
     
     public boolean isPalindrome(String str) {
@@ -44,17 +52,20 @@ class Solution {
         return true;
     }
     
-    public  boolean check(List<String> strings) {
+    public boolean check(List<String> strings) {
         for (String str : strings) {
-            if(hm.containsKey(str) && hm.get(str) == false){return false;}
-            if(hm.containsKey(str)){continue;}
-            if (!isPalindrome(str)) {
-                hm.put(str,false);
-                return false;
+            if (hm.containsKey(str)) {
+                if (!hm.get(str)) {
+                    return false;
+                }
+            } else {
+                boolean isPalin = isPalindrome(str);
+                hm.put(str, isPalin);
+                if (!isPalin) {
+                    return false;
+                }
             }
-            else{hm.put(str,true);}
         }
         return true;
     }
-    
 }
