411a6c3e053c632281a35f5c1fdf5022ce0f7d1f
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-06-29 03:52:46 +0530

    Time: 9 ms (50.80%), Space: 63.5 MB (40.11%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,23 +1,14 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        HashMap<Integer,Long>hm = new HashMap<>();
+        long[]arr = new long[n];
+        Arrays.fill(arr,0);
         for(int i = 0;i<roads.length;i++){
-            for(int j = 0;j<roads[0].length;j++){
-                hm.put(roads[i][j],hm.getOrDefault(roads[i][j],0L)+1);
-            }
+            arr[roads[i][0]] = arr[roads[i][0]]+1;
+            arr[roads[i][1]] = arr[roads[i][1]]+1;
         }
-        
-        List<Map.Entry<Integer, Long>> list = new ArrayList<>(hm.entrySet());
-        
-        list.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
-        
-        LinkedHashMap<Integer, Long> sortedHm = new LinkedHashMap<>();
-        for (Map.Entry<Integer, Long> entry : list) {
-            sortedHm.put(entry.getKey(), entry.getValue());
-        }
-        int j = n;long sum = 0;
-        for (Map.Entry<Integer, Long> entry : sortedHm.entrySet()) {
-            sum+= entry.getValue()*j;j--;
+        Arrays.sort(arr);long sum = 0;
+        for(int i = arr.length-1;i>=0;i--){
+            sum += arr[i]*n--;
         }
         return sum;
     }
