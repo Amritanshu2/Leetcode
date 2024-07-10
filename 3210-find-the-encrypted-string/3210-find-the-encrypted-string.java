671cc35dd4ffba81ade05fb3c40ed3008c4f5cf4
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-07-10 23:06:27 +0530

    Time: 7 ms (13.58%), Space: 43.7 MB (14.88%) - LeetHub

--- 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -1,19 +1,9 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
-        Queue<Character>pq = new LinkedList<>();
+        String a = "";
         for(int i = 0;i<s.length();i++){
-            pq.add(s.charAt(i));
+            a += s.charAt((i+k)%s.length());
         }
-        for(int i = 0;i<k;i++){
-            char a = pq.remove();
-            pq.add(a);
-        }
-        String sa = "";
-        for(int i = 0;i<s.length();i++){
-            char a = pq.remove();
-            sa += a;
-            pq.add(a);
-        }
-        return sa;
+        return a;
     }
 }
