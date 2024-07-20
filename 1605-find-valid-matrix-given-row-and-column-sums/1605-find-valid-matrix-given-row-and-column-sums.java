afb05d1cca65edbf90fe769c3184f9c6a9176680
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-07-20 08:43:29 +0530

    Time: 7 ms (52.59%), Space: 52.9 MB (41.43%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -1,26 +1,17 @@
 class Solution {
-    int[][]arr;
-    
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        arr= new int[rowSum.length][colSum.length];
-        fillMatrix(rowSum,colSum,0,0);
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] arr = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Math.min(rowSum[i], colSum[j]);
+                rowSum[i] -= arr[i][j];
+                colSum[j] -= arr[i][j];
+            }
+        }
+        
         return arr;
-    }
-    
-    public boolean fillMatrix(int[] rowSum, int[] colSum, int i, int j){
-        if (i >= rowSum.length || j >= colSum.length) {
-            return true;
-        }
-
-        int val = Math.min(rowSum[i], colSum[j]);
-        arr[i][j] = val;
-        rowSum[i] -= val;
-        colSum[j] -= val;
-
-        if (j == colSum.length - 1) {
-            return fillMatrix(rowSum, colSum, i + 1, 0);
-        } else {
-            return fillMatrix(rowSum, colSum, i, j + 1);
-        }
     }
 }
