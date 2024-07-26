d0420e67cf45aa4b73b61a97314e302c25c8788f
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-07-26 10:24:12 +0530

    Time: 328 ms (95.62%), Space: 0B (100.00%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COALESCE(MAX(num), NULL) AS num
+FROM 
+    (SELECT 
+         num
+     FROM 
+         MyNumbers
+     GROUP BY 
+         num
+     HAVING 
+         COUNT(num) = 1) AS singles;
