b71da50ee0d86eacf43f96a74558b5bbcf883e5b
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-10-23 14:38:00 +0530

    Time: 16 ms (71.72%), Space: 45 MB (22.92%) - LeetHub

--- 3325-count-substrings-with-k-frequency-characters-i/3325-count-substrings-with-k-frequency-characters-i.java
+++ 3325-count-substrings-with-k-frequency-characters-i/3325-count-substrings-with-k-frequency-characters-i.java
@@ -1,15 +1,37 @@
+import java.util.HashMap;
+
 class Solution {
     public int numberOfSubstrings(String s, int k) {
         int count = 0;
-        for(int i = 0;i<s.length();i++){
-            HashMap<Character,Integer>hm = new HashMap<>();
-            boolean t = false;
-            for(int j = i;j<s.length();j++){
-                hm.put(s.charAt(j),hm.getOrDefault(s.charAt(j),0)+1);
-                if(hm.get(s.charAt(j)) >= k){t = true;}
-                if(t == true){count++;}
+        int left = 0;
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            charCount.put(s.charAt(right), charCount.getOrDefault(s.charAt(right), 0) + 1);
+
+            
+            while (isValid(charCount, k)) {
+                
+                count += s.length() - right;
+
+                
+                charCount.put(s.charAt(left), charCount.get(s.charAt(left)) - 1);
+                if (charCount.get(s.charAt(left)) == 0) {
+                    charCount.remove(s.charAt(left));
+                }
+                left++;
             }
         }
+
         return count;
+    }
+
+    private boolean isValid(HashMap<Character, Integer> charCount, int k) {
+        for (int count : charCount.values()) {
+            if (count >= k) {
+                return true; 
+            }
+        }
+        return false;
     }
 }
