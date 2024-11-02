c6b9293bb908d0c4f6d15c89cef5c863ad9fc5dc
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-11-02 21:42:55 +0530

    Time: 1840 ms (5.07%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,2 +1,10 @@
 # Write your MySQL query statement below
-select s.name, SUM(COALESCE(s1.distance, 0)) as travelled_distance from  Users s left join Rides s1 on s.id = s1.user_id  group by s1.user_id order by travelled_distance desc ,s.name asc;
+# select s.name, SUM(COALESCE(s1.distance, 0)) as travelled_distance from  Users s left join Rides s1 on s.id = s1.user_id  group by s1.user_id order by travelled_distance desc ,s.name asc;
+SELECT s.name,COALESCE(
+    (SELECT SUM(s1.distance)
+                 FROM Rides s1
+                 WHERE s.id = s1.user_id
+                 GROUP BY s1.user_id),
+    0) AS travelled_distance
+FROM Users s
+ORDER BY travelled_distance DESC, s.name ASC;
