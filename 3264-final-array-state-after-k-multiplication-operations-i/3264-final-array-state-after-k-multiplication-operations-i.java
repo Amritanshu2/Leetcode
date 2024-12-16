2503ef93b5c23c8b0d7eb764bd26bf8c36db6478
Amritanshu <91659575+Amritanshu2@users.noreply.github.com>
2024-12-16 09:52:53 +0530

    Time: 4 ms (16.55%), Space: 45.1 MB (20.61%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,44 +1,59 @@
+import java.util.PriorityQueue;
+
 class Solution {
-    
+
     public class Num implements Comparable<Num> {
-    private int val;
-    private int pos;
+        private long val;
+        private int pos;
 
-    public Num(int val, int pos) {
-        this.val = val;
-        this.pos = pos;
-    }
-
-    @Override
-    public int compareTo(Num other) {
-        
-        int valComparison = Integer.compare(this.val, other.val);
-        if (valComparison != 0) {
-            return valComparison;
+        public Num(long val, int pos) {
+            this.val = val;
+            this.pos = pos;
         }
 
-        
-        return Integer.compare(this.pos, other.pos);
+        @Override
+        public int compareTo(Num other) {
+            int valComparison = Long.compare(this.val, other.val);
+            if (valComparison != 0) {
+                return valComparison;
+            }
+            return Integer.compare(this.pos, other.pos);
+        }
     }
-    }
-    
-    
+
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<Num>pq = new PriorityQueue<>();
-        for(int i = 0;i<nums.length;i++){
-            pq.add(new Num(nums[i],i));
+        PriorityQueue<Num> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(new Num(nums[i], i));
         }
-        
-        while(k>0){
-            Num a = pq.remove();
-            a.val = a.val*multiplier;
+
+        while (k > 0) {
+            Num minNum = pq.poll();
+            long updatedVal = minNum.val * (long) multiplier; // Safe multiplication with explicit casting
+            pq.add(new Num(updatedVal, minNum.pos)); // Reinsert a new object with updated value
             k--;
-            pq.add(a);
         }
-        for(int i = 0;i<nums.length;i++){
-            Num temp = pq.remove();
-            nums[temp.pos] = temp.val;
+
+        int MOD = 1_000_000_007;
+        while (!pq.isEmpty()) {
+            Num temp = pq.poll();
+            nums[temp.pos] = (int) (temp.val % MOD); // Apply modulo operation
         }
+
         return nums;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {161209470, 56851412, 39846};
+        int k = 2;
+        int multiplier = 2;
+
+        int[] result = solution.getFinalState(nums, k, multiplier);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
